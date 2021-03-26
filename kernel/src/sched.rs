4b518d69bac8900703a6ae3c4746145c5aa79813
Kamran Khan <kamranrkhan@google.com>
2021-03-26 14:30:11 -0700

    Apply OpenSK patch 05-mpu-fix

--- kernel/src/sched.rs
+++ kernel/src/sched.rs
@@ -632,6 +632,7 @@ impl Kernel {
                     let context_switch_reason = process.switch_to();
                     scheduler_timer.disarm();
                     chip.mpu().disable_app_mpu();
+                    chip.mpu().clear_mpu();
 
                     // Now the process has returned back to the kernel. Check
                     // why and handle the process as appropriate.
